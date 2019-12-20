2e959d772d89ef98f805368fcbcfb422e8964254
Cyril Dever <cdever@edgewhere.fr>
2019-12-20 11:23:05 +0100

    encryption algorithms

---
+++ lib/src/typescript/crypto/rsa/index.ts
@@ -0,0 +1,31 @@
+const forge = require('node-forge')
+const pki = forge.pki
+
+export const RSA_ALGORITHM = 'rsa'
+
+/**
+ * Encrypt a message with RSA-OAEP and returns the base64-encoded cipher text
+ * 
+ * @param msg 
+ * @param publicKey 
+ */
+export const encrypt = (msg: Buffer, publicKey: Buffer): string => {
+    const pubkey = pki.publicKeyFromPem(publicKey)
+    const crypted = pubkey.encrypt(msg, 'RSA-OAEP', {
+        md: forge.md.sha512.create()
+    })
+    return forge.util.encode64(crypted)
+}
+
+/**
+ * Decrypt a base64-encoded ciphertext
+ * 
+ * @param msg 
+ * @param privateKey
+ */
+export const decrypt = (msg: string, privateKey: Buffer): string => {
+    const privkey = pki.privateKeyFromPem(privateKey)
+    return privkey.decrypt(forge.util.decode64(msg), 'RSA-OAEP', {
+        md: forge.md.sha512.create()
+    })
+}

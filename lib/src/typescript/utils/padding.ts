3ca247031b65f2477cd733089795dacdcd94f41f
Cyril Dever <cdever@edgewhere.fr>
2020-01-24 09:25:27 +0100

    use native padding

--- lib/src/typescript/utils/padding.ts
+++ lib/src/typescript/utils/padding.ts
@@ -4,32 +4,18 @@ export const LEFT_PADDING_CHARACTER = '\u0002'
 // Unicode U+0003: end of text
 export const RIGHT_PADDING_CHARACTER = '\u0003'
 
-export const leftPad = (str: string, minLength: number): string => {
-    if (str.length >= minLength) {
-        return str
-    }
-    while (str.length < minLength) {
-        str = LEFT_PADDING_CHARACTER + str
-    }
-    return str
-}
+export const leftPad = (str: string, minLength: number): string =>
+  str.padStart(minLength, LEFT_PADDING_CHARACTER)
 
-export const rightPad = (str: string, minLength: number): string => {
-    if (str.length >= minLength) {
-        return str
-    }
-    while (str.length < minLength) {
-        str += RIGHT_PADDING_CHARACTER
-    }
-    return str
-}
+export const rightPad = (str: string, minLength: number): string =>
+  str.padEnd(minLength, RIGHT_PADDING_CHARACTER)
 
 export const unpad = (str: string): string => {
-    while (str.startsWith(LEFT_PADDING_CHARACTER)) {
-        str = str.substr(1)
-    }
-    while (str.endsWith(RIGHT_PADDING_CHARACTER)) {
-        str = str.substr(0, str.length - 1)
-    }
-    return str
+  while (str.startsWith(LEFT_PADDING_CHARACTER)) {
+    str = str.substr(1)
+  }
+  while (str.endsWith(RIGHT_PADDING_CHARACTER)) {
+    str = str.substr(0, str.length - 1)
+  }
+  return str
 }

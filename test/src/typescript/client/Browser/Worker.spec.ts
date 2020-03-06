83bfe62856a8a73d6a3f4a8ef65d2a34e5417408
Cyril Dever <cdever@edgewhere.fr>
2020-03-06 17:20:12 +0100

    add more tests to be compliant with crumbl-exe

--- test/src/typescript/client/Browser/Worker.spec.ts
+++ test/src/typescript/client/Browser/Worker.spec.ts
@@ -18,5 +18,23 @@ describe('Worker', () => {
       const found = await workerExtractor.extract(user, false)
       found.should.equal(expected)
     })
+    // Equivalent to 'crumble-exe/client/worker_test.go' tests
+    it('should decipher data with everything needed', async() => {
+      const expected = 'cdever@edgewhere.fr'
+      const crumbled = '580fb8a91f05833200dea7d33536aaec9d7ceb256a9858ee68e330e126ba409d0000a8BJM2I8mS/bkFNdZOATg8jHsQbzYp4o5rTqYWkf/pqgvkH7a4OijBxy86W1y2J+pB525jYO4iBuig2JswBdNv++8dkb0GcSXT873M0I5Xma9oM83eHXihOF2rqnqWN/RNZPwJSM23DcCj/xyVs1FK5jWVMGxtMLttIN7vqg==010158KbcQ6boXhkGdXR97+UwSHvt12wEwkVa57e+2m+66sTu32luP00cWET2gb01tgNZYjU621U7u4RI6fmz5kkyTSZtjPJ5wXISTf2wOBv5cY94LvgYoyMFKP9J3mGbPgAKGGsIdY4GCQBx6+Gi7VzfuNxdP1YHAPqcpKXPWiY+nmqYhT7eZVZlmNF1UmkMbgrneYglenmKxWSyUA6P7yMj3LrhlKekWAPdWpMLzRftLh1oH5e2KHkz7Wyh9eYOCKXlQ4sUUm8o3i0Inann41wL0KGaNajPU1RP0M9n3/Zil1/T+ZZcNJgSlQh1mxVKX1ztBRqYNUy+pqDat1qq6ED5r5A==0200a8BIIMyYgouCq7ZVy7S1kRJUl1Lg+aQMHoNeo7SauKwsy//XZ5rJOF4FrYMXmPpu0pf7nwCgAgk6Iv9IQK+WXsKpDE+QazdPpYFtxm4/1qi8qnzG1Wp/9Lf5nFTozacHqghz2e7XkaO1qyLNfmzimpsm6aw/lhEsd+djJ8KA==.1'
+      const partialUncrumb1 = '580fb8a91f05833200dea7d33536aaec9d7ceb256a9858ee68e330e126ba409d%01AgICBFUDDk4PXEc=.1'
+      const partialUncrumb2 = '580fb8a91f05833200dea7d33536aaec9d7ceb256a9858ee68e330e126ba409d%02AgICAgICAgkYUkI=.1'
+      const user: Signer = {
+        encryptionAlgorithm: ECIES_ALGORITHM,
+        privateKey: Buffer.from('b9fc3b425d6c1745b9c963c97e6e1d4c1db7a093a36e0cf7c0bf85dc1130b8a0', 'hex')
+      }
+      const workerExtractor = new BrowserWorker({
+        mode: EXTRACTION,
+        data: [crumbled, partialUncrumb1, partialUncrumb2],
+        verificationHash: '580fb8a91f05833200dea7d33536aaec9d7ceb256a9858ee68e330e126ba409d'
+      })
+      const found = await workerExtractor.extract(user, true)
+      found.should.equal(expected)
+    })
   })
 })

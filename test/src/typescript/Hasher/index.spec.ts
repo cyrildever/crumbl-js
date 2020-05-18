261c5898e5e7befcdc90cbb4b4ddbb653dcb4e5c
Cyril Dever <cdever@edgewhere.fr>
2020-05-18 15:52:18 +0200

    versions and eslint

--- test/src/typescript/Hasher/index.spec.ts
+++ test/src/typescript/Hasher/index.spec.ts
@@ -22,8 +22,8 @@ describe('Hasher', () => {
       found.should.equal(expected)
     })
     it('should work like in Go', () => {
-      const expected = "cc8b00be1cc7592806ba4f8fe4411d3744121dc12e6201dc36e873f7fd9a6bae"
-      const hashered = "cc8b00be1cc7592806ba4f8fe4411d374064d57e96845c04ed360d0901d64b7a"
+      const expected = 'cc8b00be1cc7592806ba4f8fe4411d3744121dc12e6201dc36e873f7fd9a6bae'
+      const hashered = 'cc8b00be1cc7592806ba4f8fe4411d374064d57e96845c04ed360d0901d64b7a'
       const crumbs = [
         new Crumb(new Base64('BHbIv7jmXdjb3n7+/Ewg1Br4EXYksmb4RmavTQsDVJAFY/he1rs7gpRY9+waPCww3YMzr5IIWFYN8LDVDDxV2bdTGBNjHWbsb4WRM2ItLFVYubxBto/6pfGyXJ0ZriYjL2RCVtPw8cdJrkeXqSqOEz1ICBTN9UOBZw=='), 0, 164),
         new Crumb(new Base64('BLaggeNjl7tsFhAymEipkuIco8xG8trm6E+4WzrTX7spC0DmdyTFhU8enJV2WERxOaF7iy6E64/2+2wIL/MqRMnyRnJJJDab6d7pTWMSEVOUAvEFugTP95XpnnZcDyzLdZ2J+YBbMYtRcnxH4TJA9AIwksl0oJRA/w=='), 0, 164)
